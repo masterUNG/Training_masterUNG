8b601907fdb66a15d4ae4182654cd27e5bc1cf5d
masterUNG <phrombutr@gmail.com>
2015-01-11 12:18:27 +0700

    ทดสอบการ เขียน Test
    ทดสอบการ เขียน Test

--- BoteTraining/app/src/androidTest/java/appewtc/masterung/botetraining/MainActivityTest.java
+++ BoteTraining/app/src/androidTest/java/appewtc/masterung/botetraining/MainActivityTest.java
@@ -1,11 +1,32 @@
 package appewtc.masterung.botetraining;
 
 import android.test.ActivityInstrumentationTestCase2;
+import android.widget.TextView;
 
 public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity>{
 
-    public MainActivityTest(Class<MainActivity> activityClass) {
-        super(activityClass);
-        
+    private MainActivity mActivity;
+
+    public MainActivityTest() {
+        super(MainActivity.class);
+
     } // MainActivityTest
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+
+        setActivityInitialTouchMode(false);
+        mActivity = getActivity();
+
+    } //setUp
+
+    public void testHelloWorld() {
+
+        final TextView t = (TextView) mActivity.findViewById(R.id.txtHello);
+        assertEquals("Hello master", t.getText().toString());
+
+    }   // testHelloWorld
+
+
 }   // Main Class

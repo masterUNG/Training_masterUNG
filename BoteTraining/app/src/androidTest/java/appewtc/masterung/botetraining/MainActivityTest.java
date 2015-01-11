7e7c1d8c1d29a60fd9ba28a448792985ef8e1bfc
masterUNG <phrombutr@gmail.com>
2015-01-11 11:28:04 +0700

    Create Test
    Create Test

---
+++ BoteTraining/app/src/androidTest/java/appewtc/masterung/botetraining/MainActivityTest.java
@@ -0,0 +1,11 @@
+package appewtc.masterung.botetraining;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MainActivityTest extends ActivityInstrumentationTestCase2<MainActivity>{
+
+    public MainActivityTest(Class<MainActivity> activityClass) {
+        super(activityClass);
+        
+    } // MainActivityTest
+}   // Main Class

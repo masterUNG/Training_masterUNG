a6e7997ed8445a4f60811785812c238303e8881a
masterUNG <phrombutr@gmail.com>
2015-01-13 08:24:13 +0700

    Ishihara day1
    โค้ดวันแรกครับ

--- MyIshihara_11Jan2015/app/src/main/java/appewtc/masterung/myishihara/MainActivity.java
+++ MyIshihara_11Jan2015/app/src/main/java/appewtc/masterung/myishihara/MainActivity.java
@@ -1,18 +1,67 @@
 package appewtc.masterung.myishihara;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //Explicit
+    private TextView txtQuestion;
+    private ImageView imvIshihara;
+    private RadioGroup ragChoice;
+    private RadioButton radChoice1, radChoice2, radChoice3, radChoice4;
+    private Button btnAnswer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        //Initial Widget
+        initialWidget();
+
+        //button Controller
+        buttonController();
+
+    }   // onCreate
+
+    private void buttonController() {
+
+        btnAnswer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                MediaPlayer soundButton = MediaPlayer.create(getBaseContext(), R.raw.effect_btn_long);
+                soundButton.start();
+
+            }   // event
+        });
+
+    }   // buttonController
+
+    private void initialWidget() {
+
+        txtQuestion = (TextView) findViewById(R.id.textView2);
+        imvIshihara = (ImageView) findViewById(R.id.imageView);
+        ragChoice = (RadioGroup) findViewById(R.id.radChoice);
+        radChoice1 = (RadioButton) findViewById(R.id.radioButton);
+        radChoice2 = (RadioButton) findViewById(R.id.radioButton2);
+        radChoice3 = (RadioButton) findViewById(R.id.radioButton3);
+        radChoice4 = (RadioButton) findViewById(R.id.radioButton4);
+        btnAnswer = (Button) findViewById(R.id.button);
+
+    }   // initialWidget
 
 
     @Override
@@ -36,4 +85,4 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-}
+}   // Main Class
